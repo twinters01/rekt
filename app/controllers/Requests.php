603c11d104a28bdab2b18f03ce3428bf9df446dc
Tyler Winters <twinters01@hotmail.com>
2018-07-08 17:17:54 -0400

    No need to use flashRedirect here, only using the redirect parameter

--- app/controllers/Requests.php
+++ app/controllers/Requests.php
@@ -16,7 +16,7 @@ class Requests extends Controller
   {
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-      flashRedirect('400');
+      redirect('400');
     } else {
       loginOrRedirect();
 
